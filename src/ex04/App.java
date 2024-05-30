4717cccf5ca0d5032c79e48f97060cefc672f561
lhyeonnit <lhyeonnit@gmail.com>
2024-05-30 15:15:13 +0900

    템플릿 메소드 패ì턴

---
+++ src/ex04/App.java
@@ -0,0 +1,11 @@
+package ex04;
+
+import ex04.teacher.JavaTeacher;
+import ex04.teacher.Teacher;
+
+public class App {
+    public static void main(String[] args) {
+        Teacher jt = new JavaTeacher();
+        jt.수업하기();
+    }
+}
